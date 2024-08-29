b95a5426caf86dcb73975375146c0362e4941dcf
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-08-29 13:20:18 -0400

    Skeleton done
    Set up the app with most of what I want to start with

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './styles.css';
+import '../styles.css';
 
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; 2024 [Your Name]. All rights reserved.</p>
+            <p>&copy; 2024 [James Warren]. All rights reserved.</p>
             <p>Follow me on 
-                <a href="#">LinkedIn</a>, 
-                <a href="#">GitHub</a>, 
-                <a href="#">Twitter</a>
+                <a href="www.linkedin.com/in/jameswarren83">LinkedIn</a>, 
+                <a href="https://github.com/Shadow-of-Drax">GitHub</a>, 
+                <a href="https://x.com/James_R_Warren">X</a>
             </p>
         </footer>
     );
--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './styles.css';
+import '../styles.css';
 
 const Hero = () => {
     return (
         <section id="home" className="hero">
             <div className="hero-content">
-                <h1>Hello, I'm [Your Name]</h1>
+                <h1>Hello, I'm [James Warren]</h1>
                 <p>A Web Developer passionate about creating beautiful and functional websites.</p>
                 <a href="#portfolio" className="btn">View My Work</a>
             </div>
