197548006242412d8475381154f30968266e40ce
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-08-29 16:13:45 -0400

    Update Portfolio.jsx

--- my-portfolio/src/components/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio.jsx
@@ -13,7 +13,7 @@ const Portfolio = () => {
                 </div>
                 <div className="portfolio-item">
                     <img src="project2.jpg" alt="Project 2" />
-                    <h3>Project Title 2</h3>
+                    <h3>Projects in the Works</h3>
                     <p>Short description of the project.</p>
                 </div>
                 <div className="portfolio-item">
