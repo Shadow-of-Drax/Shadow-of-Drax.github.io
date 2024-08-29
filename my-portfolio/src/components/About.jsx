ea3e7d657ea62ec3bd6a68af7c113b280a007d64
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-08-29 14:38:55 -0400

    Updating About
    Also added css

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
         <section id="about" className="about">
             <div className="about-content">
                 <h2>About Me</h2>
-                <p>My name is James Warren and I am a full stack developer. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I am a quick learner and enjoy working on projects that challenge me.</p>
+                <p>Web Developer familure with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I am a quick learner and enjoy working on projects that challenge me.
+                    Retired from the US Army after 22 years of service. I am a dedicated and hardworking individual who is always looking for ways to improve my skills and knowledge.
+                </p>
             </div>
         </section>
     );
