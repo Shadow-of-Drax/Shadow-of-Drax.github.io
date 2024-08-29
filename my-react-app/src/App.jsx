ff6346d9f858bc812dc549ea6a896e58ddc5b0fc
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-08-29 11:29:31 -0400

    Fixed bug
    Typo corrected changed from uppercase to lower case

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
